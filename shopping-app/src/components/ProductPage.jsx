d6df7f7253cb1d507a0ce7705986267a17cd3eb7
megautam@git <megautam-git>
2019-09-22 13:26:58 +0530

    adding components

---
+++ shopping-app/src/components/ProductPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class ProductPage extends Component {
+    render() {
+        return (
+            <div style={{float:"left",width:"100%",height:"500px",background:"blue"}}>hi gautam</div>
+             
+        )
+    }
+}
